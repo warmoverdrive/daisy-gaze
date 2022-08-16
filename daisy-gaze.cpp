4664d3f67b024072587593cf6a3e5601bc2166c9
WarmOverdrive <hankatherton@warmoverdrive.com>
2022-08-15 19:19:47 -0700

    added delay line with modulation loosely based off tnatoli's Sonic Daisy repo mixed with chorus via crossfade freqency of pitch shift coupled with chorus freq depth of pitch slide coupled with chorus depth scaled feedback of delay coupled with reverb feedback, scaled. Needs work.

--- daisy-gaze.cpp
+++ daisy-gaze.cpp
@@ -3,13 +3,24 @@
 #include "daisy_seed.h"
 #include "daisysp.h"
 
+// max delay time, set by multiplying the sample rate (48khz) by the seconds.
+// currently set to a max of 1 second.
+#define MAX_DELAY static_cast<size_t>(48000 * 1.f)
+
 using namespace daisy;
 using namespace daisysp;
 
 DaisySeed hw;
 ReverbSc DSY_SDRAM_BSS verb;
 Chorus DSY_SDRAM_BSS chorus;
-CrossFade cFadeFinal, cFadeMod;
+DelayLine<float, MAX_DELAY> DSY_SDRAM_BSS delayMem;
+Oscillator lfo;
+// Final mix crossfad
+CrossFade cFadeFinal;
+// Modulation/Dry Reverb crossfade
+CrossFade cFadeMod;
+// Delay/Chorus crossfade
+CrossFade cFadeModFX;
 
 u_int8_t counter;
 
@@ -19,13 +30,45 @@ enum controls
 	depth,
 	blend,
 	vol,
-	chrsFreq,
+	modFreq,
 	modBlend,
 	END_CONTROLS
 };
 
+// Pulled from tnatoli's rhythm delay code to cleanly affect the delay time modulation
+// May refactor into a class to fit the rest of the API's paradigm
+struct Delay
+{
+	DelayLine<float, MAX_DELAY> *delay;
+	float currentDelay;
+	float delayTarget;
+	float feedback;
+
+	// Reads/writes to delay line and adjusts delay time to interpolate changes in time
+	float Process(float in)
+	{
+		// smooths out the delay time delta when the target is changed
+		fonepole(currentDelay, delayTarget, 0.0002f);
+		delay->SetDelay(currentDelay);
+		float read = delay->Read();
+		delay->Write((feedback * read) + in);
+		return read;
+	}
+};
+
+Delay delay;
+float delBaseFeedback = 0.8f;
+float delChaosFeedback = 1.2f;
+float delBaseTime = MAX_DELAY * 0.25f;
+float lfo_val = 0.0f;
+float delayMaxDepth = 0.05f;
+float delayDepth = 0.0f;
+
 AnalogControl knobs[END_CONTROLS];
 Parameter param[END_CONTROLS];
+
+Switch modSwitch;	 // TEMP SWITCH FOR MODULATION TESTING
+bool isDelay = true; // TEMP BOOL FOR MODULATION TESTING
 
 /**
  * @brief
@@ -36,16 +79,18 @@ Parameter param[END_CONTROLS];
 void ProcessAnalogControls()
 {
 	// check to reset counter
-	counter = counter >= controls::END_CONTROLS ? 0 : counter;
+	counter = counter > controls::END_CONTROLS ? 0 : counter;
 
 	// every sample a new param is processed to split duties
 	switch (counter)
 	{
 	case controls::feedback:
 		verb.SetFeedback(param[feedback].Process());
+		delay.feedback = param[feedback].Value() * delBaseFeedback;
 		break;
 	case controls::depth:
 		chorus.SetLfoDepth(param[depth].Process());
+		delayDepth = param[depth].Value() * delayMaxDepth;
 		break;
 	case controls::blend:
 		cFadeFinal.SetPos(param[blend].Process());
@@ -53,11 +98,20 @@ void ProcessAnalogControls()
 	case controls::vol:
 		param[vol].Process();
 		break;
-	case controls::chrsFreq:
-		chorus.SetLfoFreq(param[chrsFreq].Process());
+	case controls::modFreq:
+		chorus.SetLfoFreq(param[modFreq].Process());
+		lfo.SetFreq(param[modFreq].Value());
 		break;
 	case controls::modBlend:
 		cFadeMod.SetPos(param[modBlend].Process());
+		break;
+	case controls::END_CONTROLS: // using this to process the delay time modulation
+		delay.delayTarget = delBaseTime * (1 + (lfo_val * delayDepth));
+
+		// temporary switch to test delay modulation
+		// modSwitch.Debounce();
+		// isDelay = modSwitch.Pressed();
+		// cFadeModFX.SetPos(isDelay ? 1.0f : 0.0f);
 		break;
 	}
 
@@ -68,22 +122,27 @@ void AudioCallback(AudioHandle::InterleavingInputBuffer in,
 				   AudioHandle::InterleavingOutputBuffer out,
 				   size_t size)
 {
-	float dry, wetRvb, wetChr, mix;
+	float dry, wetRvb, wetChr, wetDel, mix;
 
 	ProcessAnalogControls();
 
 	// loop through each sample in block
 	for (size_t i = 0; i < size; i += 2)
 	{
+		lfo_val = lfo.Process();
 		dry = in[i];
 
 		verb.Process(dry, 0, &wetRvb, 0);
 
 		wetChr = chorus.Process(wetRvb);
+		wetDel = delay.Process(wetRvb);
+
+		// Mix modulation FX
+		mix = cFadeModFX.Process(wetChr, wetDel);
 
 		// crossfade modulation amount
 		// todo set up modulating delay line
-		mix = cFadeMod.Process(wetRvb, wetChr);
+		mix = cFadeMod.Process(wetRvb, mix);
 
 		// use a crossfade to blend evenly between the dry and wet signals without changing volume
 		mix = cFadeFinal.Process(dry, mix);
@@ -111,7 +170,7 @@ void InitAnalogControls()
 	cfg[blend].InitSingle(hw.GetPin(16));	 // pin 23 red
 	cfg[feedback].InitSingle(hw.GetPin(21)); // pin 28 white
 	cfg[depth].InitSingle(hw.GetPin(22));	 // pin 29 yellow
-	cfg[chrsFreq].InitSingle(hw.GetPin(17)); // pin 24
+	cfg[modFreq].InitSingle(hw.GetPin(17));	 // pin 24
 	cfg[modBlend].InitSingle(hw.GetPin(18)); // pin 25
 	hw.adc.Init(cfg, END_CONTROLS);
 
@@ -123,10 +182,24 @@ void InitAnalogControls()
 	param[blend].Init(knobs[blend], 0.0f, 1.0f, Parameter::LINEAR);
 	param[feedback].Init(knobs[feedback], 0.75f, 0.999f, Parameter::LINEAR);
 	param[depth].Init(knobs[depth], 0.0f, 1.0f, Parameter::LINEAR);
-	param[chrsFreq].Init(knobs[chrsFreq], 0.00001f, 1.0f, Parameter::LINEAR);
+	param[modFreq].Init(knobs[modFreq], 0.001f, 1.0f, Parameter::LINEAR);
 	param[modBlend].Init(knobs[modBlend], 0.0f, 0.75f, Parameter::LINEAR);
 
 	hw.adc.Start();
+}
+
+// Set up Delay and LFO
+void InitDelay()
+{
+	delayMem.Init();
+	delay.delay = &delayMem;
+	delay.feedback = delBaseFeedback;
+	delay.currentDelay = delBaseTime;
+
+	lfo.Init(hw.AudioSampleRate());
+	lfo.SetWaveform(lfo.WAVE_SIN);
+	lfo.SetFreq(1.0f);
+	lfo.SetAmp(1.0f);
 }
 
 int main(void)
@@ -140,11 +213,16 @@ int main(void)
 	chorus.Init(hw.AudioSampleRate());
 	chorus.SetFeedback(0.4f);
 
+	InitDelay();
+
 	// init crossfades with constant power curves
 	cFadeFinal.Init(CROSSFADE_CPOW);
 	cFadeMod.Init(CROSSFADE_CPOW);
+	cFadeModFX.Init(CROSSFADE_CPOW);
 
 	InitAnalogControls();
+
+	modSwitch.Init(hw.GetPin(10)); // PIN 11
 
 	hw.StartAudio(AudioCallback);
 	while (1)
